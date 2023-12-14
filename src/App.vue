9072c2ee9d2ddb6bc67716ba25b3d2bf1b4d4965
jiu-lin <jam930725@gmail.com>
2023-12-14 15:04:57 +0800

    Move TheFooter.vue to the right folder.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TheHeader from './components/TheHeader.vue';
-import TheFooter from './components/icons/TheFooter.vue';
+import TheFooter from './components/TheFooter.vue';
 import { RouterView } from 'vue-router';
 </script>
 
