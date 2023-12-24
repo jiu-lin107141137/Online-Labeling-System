6f8af694a1c860ee906a6ad453db8ee978225d76
jiu-lin <jam930725@gmail.com>
2023-12-24 12:28:03 +0800

    Add config of DB connection, hash and JWT.

---
+++ env.ts
@@ -0,0 +1,42 @@
+import { createHash } from 'crypto';
+
+const dbConfig = {
+  user: 'Jason',
+  password: 'Jason',
+  server: '127.0.0.1', 
+  database: 'OnlineLabelingDB',
+  connectionTimeout: 10000,
+  requestTimeout: 10000,
+  options: {
+      trustedConnection: true,
+      enableArithAort: true,
+      instancename: 'SQLEXPRESS',
+      trustServerCertificate: true
+  },
+  port: 1433
+}
+
+const hashConfig = {
+  salt: 'RxjYf7pd4VbAPWHfQpDfsHEqthVrk/SiqKcxuRZIqAg=',
+  hashString: (msg: string) => {
+      return createHash("sha256").update(msg+hashConfig.salt).digest("hex");
+  }
+}
+
+const EXPIRES_IN = 20 * 60 * 1000;
+const REFRESH_EXPIRES_IN = 24 * 60 * 60 * 1000;
+const SECRET = "RxjYf7pd4VbAPWHfQpDfsHEqthVrk/SiqKcxuRZIqAg=";
+const REFRESH_SECRET = "zGmhA4vJsbRSAO8JVSohapdBD/husbYA=sf9HqLa6cja";
+
+const jwtConfig = {
+  EXPRIRES_IN : EXPIRES_IN+"ms", 
+  REFRESH_EXPIRES_IN: REFRESH_EXPIRES_IN+"ms",
+  SECRET: SECRET,
+  REFRESH_SECRET: REFRESH_SECRET,
+}
+
+module.exports = {
+  dbConfig,
+  hashConfig,
+  jwtConfig,
+};
