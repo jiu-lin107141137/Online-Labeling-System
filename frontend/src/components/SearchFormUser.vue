1b52f9170783a8ecc7c95fd09fe2e71cec73d465
Jiu-Lin <jam930725@gmail.com>
2024-04-08 16:34:42 +0800

    Add User management overview view and component.

---
+++ frontend/src/components/SearchFormUser.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <table>
+    <tr>
+      <thead>
+        User management
+      </thead>
+    </tr>
+  </table>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- frontend/src/views/UserOverviewView.vue
+++ frontend/src/views/UserOverviewView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import SearchUserForm from '@/components/SearchFormUser.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container-wrapper">
+    <div class="content-container">
+      <SearchUserForm />
+    </div>
+  </div>
 </template>
